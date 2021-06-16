aed8ffe03471784f96eaa7ecdf5693417b7b86d5
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-06-15 23:06:14 -0300

    Pagina Zoom-Range - Diseño y tarea

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container {
+      width: 100%;
+      height: 100%;
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-70.669265, -33.448890],
+      zoom: 8
+    });
   }
 
 }
